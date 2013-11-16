9cde5cb1a42a95aa2f7c4660bf64f8ee2a3fc7e1
Dhika Ahmad Aulia <dhika.ahmad.aulia@gmail.com>
2013-11-16 17:12:09 +0700

    animation now works

--- src/com/pockettrainer/MainDashboard.java
+++ src/com/pockettrainer/MainDashboard.java
@@ -4,8 +4,9 @@
 
 package com.pockettrainer;
 
+import java.text.DecimalFormat;
+
 import com.example.pockettrainer.R;
-import com.pockettrainer.animation.MainThread;
 import com.pockettrainer.animation.SpriteAnimation;
 
 import android.content.Context;
@@ -16,6 +17,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
@@ -32,12 +35,44 @@ public class MainDashboard extends SurfaceView implements
 
 	private static final String TAG = MainDashboard.class.getSimpleName();
 
-	private BackgroundTask bgTask;
-	private MainThread thread;
+	// desired fps
+	private final static int MAX_FPS = 50;
+	// maximum number of frames to be skipped
+	private final static int MAX_FRAME_SKIPS = 5;
+	// the frame period
+	private final static int FRAME_PERIOD = 1000 / MAX_FPS;
+
+	/* Stuff for stats */
+	private DecimalFormat df = new DecimalFormat("0.##"); // 2 dp
+	// we'll be reading the stats every second
+	private final static int STAT_INTERVAL = 1000; // ms
+	// the average will be calculated by storing
+	// the last n FPSs
+	private final static int FPS_HISTORY_NR = 10;
+	// the status time counter
+	private long statusIntervalTimer = 0l;
+	// number of frames skipped since the game started
+	private long totalFramesSkipped = 0l;
+	// number of frames skipped in a store cycle (1 sec)
+	private long framesSkippedPerStatCycle = 0l;
+
+	// number of rendered frames in an interval
+	private int frameCountPerStatCycle = 0;
+	private long totalFrameCount = 0l;
+	// the last FPS values
+	private double fpsStore[];
+	// the number of times the stat has been read
+	private long statsCount = 0;
+	// the average FPS since the game started
+	private double averageFps = 0.0;
+	// Surface holder that can access the physical surface
+
+	private Handler customHandler = new Handler();
 	private SpriteAnimation sprite;
 
 	private boolean isTouched;
-	
+	boolean mSurfaceExists = true;
+
 	private BitmapDrawable tile;
 
 	// the fps to be displayed
@@ -59,11 +94,11 @@ public class MainDashboard extends SurfaceView implements
 		Display display = wm.getDefaultDisplay();
 		Point size = new Point();
 		display.getSize(size);
-		
+
 		// create Elaine and load bitmap
 		sprite = new SpriteAnimation(BitmapFactory.decodeResource(
-				getResources(), R.drawable.sprite_egg), size.x / 2,
-				200 // initial position
+				getResources(), R.drawable.sprite_egg), size.x / 2, 200 // initial
+																		// position
 				, 300, 300 // width and height of sprite
 				, 30, 20 // FPS and number of frames in the animation
 				, true);
@@ -74,8 +109,6 @@ public class MainDashboard extends SurfaceView implements
 
 		// create the game loop thread
 		// thread = new MainThread(getHolder(), this);
-
-		bgTask = new BackgroundTask(getHolder(), this);
 
 		// make the GamePanel focusable so it can handle events
 		setFocusable(true);
@@ -91,43 +124,23 @@ public class MainDashboard extends SurfaceView implements
 		// at this point the surface is created and
 		// we can safely start the game loop
 		Log.i("start", "resuming " + ThreadIsRunning);
-		if (!ThreadIsRunning) {
-			bgTask.execute();
-			ThreadIsRunning = true;
-		} else {
-			bgTask.resumeTask();
-		}
-
-		// start();
-	}
-
-	public void start() {
-		if (!ThreadIsRunning) {
-			thread.setRunning(true);
-			thread.start();
-			ThreadIsRunning = true;
-		} else {
-			thread.setRunning(true);
-			thread.onResume();
-		}
+		customHandler.postDelayed(taskRunnable, 0);
+		ThreadIsRunning = true;
+		// if (bgTask == null || bgTask.getStatus() ==
+		// AsyncTask.Status.FINISHED)
+		// {
+		// bgTask = new BackgroundTask(getHolder(), this);
+		// }
+		// mSurfaceExists = true;
+		// bgTask.execute(AndEngineTexturePackerExtension
+		// AndEngineTexturePackerExtension );
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		Log.d(TAG, "Surface is being destroyed");
-		bgTask.cancelTask();
-		// tell the thread to shut down and wait for it to finish
-		// this is a clean shutdown
-		// boolean retry = true;
-		// while (retry) {
-		// try {
-		// thread.join();
-		// retry = false;
-		// } catch (InterruptedException e) {
-		//
-		// }
-		// }
-		// Log.d(TAG, "Thread was shut down cleanly");
+		// mSurfaceExists = false;
+		customHandler.removeCallbacks(taskRunnable);
 	}
 
 	@Override
@@ -148,7 +161,7 @@ public class MainDashboard extends SurfaceView implements
 				sprite.goMove();
 			}
 		} else if (event.getAction() == MotionEvent.ACTION_UP) {
-			if(isTouched) {
+			if (isTouched) {
 				isTouched = false;
 				sprite.goEnd();
 			}
@@ -173,16 +186,11 @@ public class MainDashboard extends SurfaceView implements
 	}
 
 	public void pauseThread() {
-		// bgTask.setRunning(false);
-		bgTask.pauseTask();
-		// thread.setRunning(false);
-		// thread.onPause();
+		customHandler.removeCallbacks(taskRunnable);
 	}
 
 	public void resumeThread() {
-		// bgTask.setRunning(true);
-		bgTask.resumeTask();
-		// thread.setRunning(true);
+//		customHandler.postDelayed(taskRunnable, 0);
 	}
 
 	private void displayFps(Canvas canvas, String fps) {
@@ -191,6 +199,110 @@ public class MainDashboard extends SurfaceView implements
 			paint.setARGB(255, 255, 255, 255);
 			canvas.drawText(fps, this.getWidth() - 50, 20, paint);
 		}
+	}
+
+	private Runnable taskRunnable = new Runnable() {
+
+		@Override
+		public void run() {
+			Canvas canvas;
+			Log.d(TAG, "Starting game loop");
+			// initialise timing elements for stat gathering
+			initTimingElements();
+
+			long beginTime; // the time when the cycle begun
+			long timeDiff; // the time it took for the cycle to execute
+			int sleepTime; // ms to sleep (<0 if we're behind)
+			int framesSkipped; // number of frames being skipped
+
+			sleepTime = 0;
+
+			canvas = getHolder().lockCanvas(null);
+			// try locking the canvas for exclusive pixel editing
+			// in the surface
+			try {
+				beginTime = System.currentTimeMillis();
+				framesSkipped = 0; // resetting the frames skipped
+				update();
+				render(canvas);
+				timeDiff = System.currentTimeMillis() - beginTime;
+				sleepTime = (int) (FRAME_PERIOD - timeDiff);
+
+				if (sleepTime > 0) {
+					// if sleepTime > 0 we're OK
+					try {
+						Thread.sleep(sleepTime);
+					} catch (InterruptedException e) {
+					}
+				}
+
+				while (sleepTime < 0 && framesSkipped < MAX_FRAME_SKIPS) {
+					update(); // update without rendering
+					sleepTime += FRAME_PERIOD; // add frame period to check
+												// if in next frame
+					framesSkipped++;
+				}
+				customHandler.postDelayed(this, 0);
+				framesSkippedPerStatCycle += framesSkipped;
+				storeStats();
+			} finally {
+				if (canvas != null) {
+					getHolder().unlockCanvasAndPost(canvas);
+				}
+			} // end finally
+		}
+
+	};
+
+	private void storeStats() {
+		frameCountPerStatCycle++;
+		totalFrameCount++;
+		// assuming that the sleep works each call to storeStats
+		// happens at 1000/FPS so we just add it up
+		statusIntervalTimer += FRAME_PERIOD;
+
+		if (statusIntervalTimer >= STAT_INTERVAL) {
+			// calculate the actual frames pers status check interval
+			double actualFps = (double) (frameCountPerStatCycle / (STAT_INTERVAL / 1000));
+
+			// stores the latest fps in the array
+			fpsStore[(int) statsCount % FPS_HISTORY_NR] = actualFps;
+
+			// increase the number of times statistics was calculated
+			statsCount++;
+
+			double totalFps = 0.0;
+			// sum up the stored fps values
+			for (int i = 0; i < FPS_HISTORY_NR; i++) {
+				totalFps += fpsStore[i];
+			}
+
+			// obtain the average
+			if (statsCount < FPS_HISTORY_NR) {
+				// in case of the first 10 triggers
+				averageFps = totalFps / statsCount;
+			} else {
+				averageFps = totalFps / FPS_HISTORY_NR;
+			}
+			// saving the number of total frames skipped
+			totalFramesSkipped += framesSkippedPerStatCycle;
+			// resetting the counters after a status record (1 sec)
+			framesSkippedPerStatCycle = 0;
+			statusIntervalTimer = 0;
+			frameCountPerStatCycle = 0;
+			// Log.d(TAG, "Average FPS:" + df.format(averageFps));
+			this.setAvgFps("FPS: " + df.format(averageFps));
+		}
+	}
+
+	private void initTimingElements() {
+		// initialise timing elements
+		fpsStore = new double[FPS_HISTORY_NR];
+		for (int i = 0; i < FPS_HISTORY_NR; i++) {
+			fpsStore[i] = 0.0;
+		}
+		Log.d(TAG + ".initTimingElements()",
+				"Timing elements for stats initialised");
 	}
 
 }
