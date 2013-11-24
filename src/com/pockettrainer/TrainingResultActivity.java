b105ef8fcc65744ca1303aeebe52c19415092578
Dhika Ahmad Aulia <dhika.ahmad.aulia@gmail.com>
2013-11-24 17:26:12 +0700

    share feature implemented

--- src/com/pockettrainer/TrainingResultActivity.java
+++ src/com/pockettrainer/TrainingResultActivity.java
@@ -1,45 +1,134 @@
 package com.pockettrainer;
 
+import java.io.File;
+
+import org.brickred.socialauth.android.DialogListener;
+import org.brickred.socialauth.android.SocialAuthAdapter;
+import org.brickred.socialauth.android.SocialAuthError;
+import org.brickred.socialauth.android.SocialAuthListener;
+import org.brickred.socialauth.android.SocialAuthAdapter.Provider;
+
 import com.example.pockettrainer.R;
 import com.example.pockettrainer.R.id;
 import com.example.pockettrainer.R.layout;
 import com.example.pockettrainer.R.menu;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.MenuItem.OnMenuItemClickListener;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class TrainingResultActivity extends Activity {
+
+	SocialAuthAdapter adapter;
+	Button share;
+	Button cont;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_training_result);
+
+		share = (Button) findViewById(R.id.share_button);
+		cont = (Button) findViewById(R.id.continue_button);
+		
+		adapter = new SocialAuthAdapter(new ResponseListener());
+		adapter.addProvider(Provider.FACEBOOK, R.drawable.facebook);
+		adapter.addProvider(Provider.TWITTER, R.drawable.twitter);
+		adapter.addCallBack(Provider.TWITTER, "http://socialauth.in/socialauthdemo/socialAuthSuccessAction.do");
+		adapter.enable(share);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		
-		menu.findItem(R.id.action_settings).setOnMenuItemClickListener(new OnMenuItemClickListener() {
 
-			@Override
-			public boolean onMenuItemClick(MenuItem item)
-			{
-//				Toast.makeText(getApplicationContext(), "Setting", Toast.LENGTH_SHORT).show();
-//				return true;
-				Intent intent = new Intent(TrainingResultActivity.this.getApplicationContext(), TestAccelerometerActivity.class);
-				startActivity(intent);
-				return true;
-			}
-		});
+		menu.findItem(R.id.action_settings).setOnMenuItemClickListener(
+				new OnMenuItemClickListener() {
+
+					@Override
+					public boolean onMenuItemClick(MenuItem item) {
+						// Toast.makeText(getApplicationContext(), "Setting",
+						// Toast.LENGTH_SHORT).show();
+						// return true;
+						Intent intent = new Intent(TrainingResultActivity.this
+								.getApplicationContext(),
+								TestAccelerometerActivity.class);
+						startActivity(intent);
+						return true;
+					}
+				});
 
 		return true;
+	}
+
+	private final class ResponseListener implements DialogListener {
+		@Override
+		public void onComplete(Bundle values) {
+
+			Log.d("ShareButton", "Authentication Successful");
+
+			// Get name of provider after authentication
+			final String providerName = values
+					.getString(SocialAuthAdapter.PROVIDER);
+			Log.d("ShareButton", "Provider Name = " + providerName);
+			
+			String message = "Pocket Trainer share button test, will be out soon!";
+
+			// Please avoid sending duplicate message. Social Media Providers
+			// block duplicate messages.
+			adapter.updateStatus(message, new MessageListener(), false);
+
+		}
+
+		@Override
+		public void onError(SocialAuthError error) {
+			Log.d("ShareButton", "Authentication Error: " + error.getMessage());
+		}
+
+		@Override
+		public void onCancel() {
+			Log.d("ShareButton", "Authentication Cancelled");
+		}
+
+		@Override
+		public void onBack() {
+			Log.d("Share-Button", "Dialog Closed by pressing Back Key");
+		}
+
+	}
+
+	// To get status of message after authentication
+	private final class MessageListener implements SocialAuthListener<Integer> {
+		@Override
+		public void onExecute(String provider, Integer t) {
+			Integer status = t;
+			if (status.intValue() == 200 || status.intValue() == 201
+					|| status.intValue() == 204)
+				Toast.makeText(TrainingResultActivity.this,
+						"Message posted on " + provider, Toast.LENGTH_LONG)
+						.show();
+			else
+				Toast.makeText(TrainingResultActivity.this,
+						"Message not posted on " + provider, Toast.LENGTH_LONG)
+						.show();
+		}
+
+		@Override
+		public void onError(SocialAuthError e) {
+
+		}
 	}
 
 }
